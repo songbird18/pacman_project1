a482189ce686fe876755ab626a4a726612ffa70d
songbird18 <98717105+songbird18@users.noreply.github.com>
2023-04-11 12:28:47 -0500

    Q2 implementation passing all tests

--- Q2_yolo/car_detect_yolo.py
+++ Q2_yolo/car_detect_yolo.py
@@ -11,6 +11,7 @@ import pandas as pd
 import PIL
 from PIL import ImageFont, ImageDraw, Image
 import tensorflow as tf
+import yolo_utils
 from tensorflow.python.framework.ops import EagerTensor
 from yolo_utils import  read_classes, read_anchors,scale_boxes, preprocess_image
 from tensorflow.keras.models import load_model
@@ -42,24 +43,32 @@ def yolo_filter_boxes(boxes, box_confidence, box_class_probs, threshold = .6):
     ### START CODE HERE
     # Step 1: Compute box scores
     ##(≈ 1 line)
-    box_scores = None
+    # multiply probability by confidence to get a full set of scores by class
+    box_scores = box_class_probs * box_confidence
 
     # Step 2: Find the box_classes using the max box_scores, keep track of the corresponding score
     ##(≈ 2 lines)
     # IMPORTANT: set axis to -1
-    box_classes = None
-    box_class_scores = None
+    # argmax returns the index of the greatest box score (i.e. the class with the greatest score)
+    # reduce_max returns the values at that index (the scores for the above described class)
+    box_classes = tf.math.argmax(box_scores, -1)
+    box_class_scores = tf.math.reduce_max(box_scores, -1)
     
     # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
     # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
     ## (≈ 1 line)
-    filtering_mask = None
+    # using the > operator compares each score in the class against the threshold to determine what is acceptable
+    # since we are comparing probability to threshold, not scores, the class scores need to be divided by confidence
+    filtering_mask = box_class_scores > threshold
+    
     
     # Step 4: Apply the mask to box_class_scores, boxes and box_classes
     ## (≈ 3 lines)
-    scores = None
-    boxes = None
-    classes = None
+    # the mask filters out scores that correspond to "false" (less than threshold)
+    # and also filters out the boxes and classes that correspond
+    scores = tf.boolean_mask(box_class_scores, filtering_mask)
+    boxes = tf.boolean_mask(boxes, filtering_mask)
+    classes = tf.boolean_mask(box_classes, filtering_mask)
     ### END CODE HERE
     
     return scores, boxes, classes
@@ -82,22 +91,27 @@ def iou(box1, box2):
     ### START CODE HERE
     # Calculate the (yi1, xi1, yi2, xi2) coordinates of the intersection of box1 and box2. Calculate its Area.
     ##(≈ 7 lines)
-    xi1 = None
-    yi1 = None
-    xi2 = None
-    yi2 = None
-    inter_width = None
-    inter_height =  None
-    inter_area = None
+    #(xi1,yi1) is the max of x1 and y1 respectively (top left corner of intersection)
+    #(xi2,yi2) is the min of x2 and y2 respectively (bottom right corner of intersection)
+    xi1 = np.maximum(box1[0], box2[0])
+    yi1 = np.maximum(box1[1], box2[1])
+    xi2 = np.minimum(box1[2], box2[2])
+    yi2 = np.minimum(box1[3], box2[3])
+    # if there is no intersection & a gap between the two boxes, default to 0 instead of accepting a negative number
+    # this avoids unintentional positive area value for no intersection
+    inter_width = max(xi2-xi1, 0)
+    inter_height = max(yi2-yi1, 0)
+    inter_area = inter_width * inter_height
     
     # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
     ## (≈ 3 lines)
-    box1_area = None
-    box2_area = None
-    union_area = None
+    box1_area = (box1[2]-box1[0]) * (box1[3]-box1[1])
+    box2_area = (box2[2]-box2[0]) * (box2[3]-box2[1])
+    union_area = box1_area + box2_area - inter_area
     
     # compute the IoU
-    iou = None
+    # cast values to float to ensure no rounding of areas
+    iou = float(inter_area)/float(union_area)
     ### END CODE HERE
     
     return iou
@@ -130,13 +144,13 @@ def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_thresho
     ### START CODE HERE
     # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
     ##(≈ 1 line)
-    nms_indices = None
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_boxes, iou_threshold)
     
     # Use tf.gather() to select only nms_indices from scores, boxes and classes
     ##(≈ 3 lines)
-    scores = None
-    boxes = None
-    classes = None
+    scores = tf.gather(scores, nms_indices)
+    boxes = tf.gather(boxes, nms_indices)
+    classes = tf.gather(classes, nms_indices)
     ### END CODE HERE
 
     
@@ -145,8 +159,8 @@ def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_thresho
 def yolo_boxes_to_corners(box_xy, box_wh):
 
     """Convert YOLO box predictions to bounding box corners."""
-    box_mins = box_xy - (box_wh / 2.)
-    box_maxes = box_xy + (box_wh / 2.)
+    box_mins = box_xy - (box_wh / 2.0)
+    box_maxes = box_xy + (box_wh / 2.0)
 
     return tf.keras.backend.concatenate([
         box_mins[..., 1:2],  # y_min
@@ -157,7 +171,7 @@ def yolo_boxes_to_corners(box_xy, box_wh):
 # UNQ_C4 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
 # GRADED FUNCTION: yolo_eval
 
-def yolo_eval(yolo_outputs, image_shape = (720, 1280), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+def yolo_eval(yolo_outputs, image_shape = (720.0, 1280.0), max_boxes=10, score_threshold=.6, iou_threshold=.5):
     """
     Converts the output of YOLO encoding (a lot of boxes) to your predicted boxes along with their scores, box coordinates and classes.
     
@@ -180,20 +194,24 @@ def yolo_eval(yolo_outputs, image_shape = (720, 1280), max_boxes=10, score_thres
     
     ### START CODE HERE
     # Retrieve outputs of the YOLO model (≈1 line)
-    box_xy, box_wh, box_confidence, box_class_probs = None
-    
+    box_xy, box_wh, box_confidence, box_class_probs = yolo_outputs
+
     # Convert boxes to be ready for filtering functions (convert boxes box_xy and box_wh to corner coordinates)
-    boxes = None
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
     
     # Use one of the functions you've implemented to perform Score-filtering with a threshold of score_threshold (≈1 line)
-    scores, boxes, classes = None
+    scores, boxes, classes = yolo_filter_boxes(boxes, box_confidence, box_class_probs, score_threshold)
     
+    #re-cast image_shape to ensure compatibility in scale_boxes call
+    image_shape = tf.cast(image_shape, dtype = tf.float32)
+
     # Scale boxes back to original image shape.
-    boxes = None
+    boxes = scale_boxes(boxes, image_shape)
     
     # Use one of the functions you've implemented to perform Non-max suppression with 
     # maximum number of boxes set to max_boxes and a threshold of iou_threshold (≈1 line)
-    scores, boxes, classes = None
+    scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes, max_boxes, iou_threshold)
+
     ### END CODE HERE
     
     return scores, boxes, classes
@@ -213,7 +231,12 @@ def predict(image_file):
     
     Note: "None" actually represents the number of predicted boxes, it varies between 0 and max_boxes. 
     """
+    # extract class names and anchors
+    class_names = read_classes("model_data/coco_classes.txt")
+    anchors = read_anchors("model_data/yolo_anchors.txt")
 
+    # load the model -->yolo_model
+    yolo_model = load_model("model_data/", compile=False)
 
     # Preprocess your image
     image, image_data = preprocess_image("images/" + image_file, model_image_size = (608, 608))
@@ -229,7 +252,7 @@ def predict(image_file):
     colors = get_colors_for_classes(len(class_names))
     # Draw bounding boxes on the image file
     #draw_boxes2(image, out_scores, out_boxes, out_classes, class_names, colors, image_shape)
-    draw_boxes(image, out_boxes, out_classes, class_names, out_scores)
+    yolo_utils.draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
     # Save the predicted bounding box on the image
     image.save(os.path.join("out", image_file), quality=100)
     # Display the results in the notebook
